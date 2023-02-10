ffbf25c3074f3d6b774b8930b63910f8ec06512b
elektrick66 <louaymoussa2001@gmail.com>
2023-02-10 01:23:53 +0100

    final Chatcord project

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,17 +17,48 @@ const io = socketio(server);
 //set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "ChatCord Bot";
+
 //run when client connects
 io.on("connection", (socket) => {
-  //Welcome current user
-  socket.emit("message", "Welcome to chatcord"); // send to 1 client
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
 
-  //Broadcast when user connects
-  socket.broadcast.emit("message", "A user has joined the chat"); //send to everyone except the one that's connecting
+    //Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to chatcord")); // send to 1 client
+
+    //Broadcast when user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      ); //.broadcast.emit sends to everyone except the one that's connecting
+
+    //Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  //Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit("message", formatMessage(`${user.username}`, msg));
+  });
 
   //Run when client disconnects
   socket.on("disconnect", () => {
-    io.emit("message", " A user has left the chat");
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+    }
   });
 
   ///io.emit(); //send to everyone
